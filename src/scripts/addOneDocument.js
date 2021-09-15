85f329b9b5302df8d6193e7e18daebd87f01445b
Kareem Zuwa <kareemzuwa@gmail.com>
2021-09-15 10:12:09 +0200

    addonedocument script done

---
+++ src/scripts/addOneDocument.js
@@ -0,0 +1,29 @@
+// Importera Firebase certifieringen och lägg i en variabel
+const { connect } = require('../database.js')
+const db = connect()
+
+// Databas Collection Hamster
+const HAMSTERS = 'hamsters'
+
+// Kalla på funktionen addOne
+addOne()
+
+async function addOne() {
+    console.log('add a new document...');
+    const hamsterObject = {
+        wins: 0,
+        games: 0,
+        age: 2,
+        favFood: 'honung',
+        id: 41,
+        imgName: 'hamster-41.jpg',
+        name: 'Smulan',
+        loves: 'lukta på blommorna',
+        defeats: 0
+        
+
+    }
+    // utan att ange id
+const docRef = await db.collection(HAMSTERS).add(hamsterObject)
+console.log('The document id is: ' + docRef.id)
+}
