df6d2c9a06478aaea4f97e365679185d80be6060
Kareem Zuwa <kareemzuwa@gmail.com>
2021-09-15 09:58:53 +0200

    added getonedocument script

---
+++ src/scripts/getOneDocument.js
@@ -0,0 +1,26 @@
+// Importera Firebase certifieringen och lägg i en variabel
+const { connect } = require('../database.js')
+const db = connect()
+
+// Databas Collection Hamster
+const HAMSTERS = 'hamsters'
+
+// kalla på funktion
+getOne();
+
+
+// Hämta "Dart" från databasen
+async function getOne(id) {
+	console.log('Looking for Dart');
+	const docId = id || '0LUjvKfuYAQON8iieS0l'
+
+	const docSnapshot = await db.collection(HAMSTERS).doc(docId).get()
+
+	if( !docSnapshot.exists ) {
+		console.log('Could not find hamster!');
+		return
+	}
+	const data = await docSnapshot.data()
+	console.log('Found: ', data);
+	return data
+}
--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ let requestCount = 0
 
 // Setup MiddleWare
 app.use( '/web', express.static(__dirname + '/../frontend') )
-app.use( express.urlencoded() )
+app.use( express.urlencoded({ extended: true }) )
 app.use( express.json() )
 app.use( (req, res, next) => {
 	requestCount++
